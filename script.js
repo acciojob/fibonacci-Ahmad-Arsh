b8f284673fd685dee29ed15471e70b109b1334c5
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-15 20:45:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,7 @@ function fibonacci(num) {
 // your code here
 	 if (num <= 0) return 0;
     if (num === 1) return 1;
-    let a = 0;
-    let b = 1;
-    for (let i = 2; i <= num; i++) {
-        let temp = b;
-        b = a + b;
-        a = temp;
-    }
-    return b;
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
